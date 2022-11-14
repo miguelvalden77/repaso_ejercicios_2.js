8dd5336d395e4df0ad65c29e429cccf716732bfc
miguelvalden <miguelvalden77@gmail.com>
2022-11-15 00:01:11 +0100

    repaso

---
+++ javascript.js
@@ -0,0 +1,49 @@
+// 1. Haz una función que reciba un array de nombres y haga un console.log() de cada uno de los nombres (forEach())
+
+
+// 2. Haz una función que reciba un array de objetos y si tiene 18 años o más haz un console.log() saludandolo por su nombre (forEach()).
+let usuarios = [
+    {
+        nombre: "Miguel",
+        edad: 22
+    },
+    {
+        nombre: "Jose",
+        edad: 13
+    },
+    {
+        nombre: "Marta",
+        edad: 18
+    },
+    {
+        nomre: "Lucia",
+        edad: 11
+    }
+]
+
+
+// 3. Haz una función que reciba un array de números y los ordene de menor a mayor y devuelva dicho array
+
+
+
+// 4. Haz una función que reciba un array de objetos, que será el mismo que en el ejercicio 2 y los ordene por edad de mayor a menor. Devuelve el array.
+
+
+
+// 5. Haz una función que reciba un array de objetos, el mismo que en el ejercicio anterior y devuelva un array con los nombres de las personas.
+
+
+
+// 6. Haz una función que reciba un array de números, y devuelva otro array con los números multiplicados por 2 si son pares y por 5 si son impares
+
+
+
+// 7. Haz una función que reciba un array de objetos (el array es el mismo que el anterior) y devuelva otro array con aquellas personas que tengan 18 años o más. 
+
+
+// 8. Haz una función que reciba un array de números y devuelva otro array con los números impares.
+
+
+// 9. Haz una función que reciba un lista de números y devuelva el menor
+
+
